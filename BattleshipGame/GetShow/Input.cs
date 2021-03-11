2c4ad99c8f277160de7cd15015e951d548e0e6e5
eorll <robert0dadon@gmail.com>
2021-03-11 16:59:47 +0100

    GetCoordinates()

--- BattleshipGame/GetShow/Input.cs
+++ BattleshipGame/GetShow/Input.cs
@@ -1,7 +1,47 @@
-﻿namespace BattleshipGame.GetShow
+﻿using System;
+using System.Text;
+using static System.Console;
+
+namespace BattleshipGame.GetShow
 {
     public class Input
     {
-        
+        // public ConsoleKeyInfo GetKeyPressed()
+        // {
+        //     return ReadKey(true);
+        // }
+
+        public (int, int) GetCoordinates()
+        {
+            int x = -1;
+            int y = -1;
+            do
+            {
+                WriteLine("Enter coordinates (e.q. A1)");
+                string userInput = ReadLine()?.ToUpper();
+                if (userInput != null)
+                {
+                    char column = userInput[0];
+
+                    if (column < 65 ^ column > 90)
+                    {
+                        WriteLine("First position should be letter from A to Z!");
+                    }
+                    else
+                    {
+                        if (int.TryParse(userInput.Substring(1), out y))
+                        {
+                            x = column - 65;
+                        }
+                        else
+                        {
+                            WriteLine("Second position should be number!");
+                        }
+                    }
+                }
+            } while (x == -1 ^ y == -1);
+
+            return (x, y);
+        }
     }
 }
